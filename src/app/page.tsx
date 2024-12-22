11dc675bd501fe13cdc2d23c8bb01e6ad221aeb8
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-12-22 23:14:38 +1100

    sign in redirect if not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import HomeScreen from "./_components/HomeScreen";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
+
+  if (!session?.user) {
+    redirect("api/auth/signin");
+  }
 
   if (session?.user) {
     void api.post.getLatest.prefetch();
