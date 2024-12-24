f8d1579973a12229b86a4c5e04078a08d99deb6e
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-12-24 18:52:55 +1100

    consistency with loading representation

--- src/app/_components/Loading.tsx
+++ src/app/_components/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center">
+    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
       <div className="loader"></div>
     </div>
   );
