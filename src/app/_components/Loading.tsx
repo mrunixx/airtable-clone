2082bbe86c9fd6532bd3dd009c59d0b93465b92a
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-12-24 18:31:57 +1100

    loading page

---
+++ src/app/_components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
